fb0cd51da36e179551f4ff91a88bd461923143d9
Damiano Chintala <damianochintala@gmail.com>
2018-04-20 16:57:54 +0200

    Adding changing Icon colours

--- src/Thumbs.jsx
+++ src/Thumbs.jsx
@@ -11,14 +11,33 @@ export class Thumbnails extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            style : {background: "blue"}
-        }
+            style: {backgroundColor:"red"},
+            status: false
+
+        };
     }
-    ChangeStyle(){
-          this.setState({
-                style : this.props.style
+
+    changeStyle(){
+        if(this.state.status===false){
+            this.setState({
+                style : {backgroundColor:"blue"},
+                status: true
             });
+
+        }else{
+            this.setState({
+                style : {backgroundColor:"red"},
+                status:false
+            });
+
         }
+      
+        
+        }
+
+    likeThis(){
+        alert('Thank You for liking this image');
+    }
 
 	render () {
 		return (
@@ -27,32 +46,32 @@ export class Thumbnails extends React.Component {
               <div className = "col-md-4 hold card">
                 <img className = "resize" src={street}/>
                 <p className = "card-text"> I am a text</p>
-                <a><button onClick = {this.ChangeStyle.bind(this)} style ={this.state.styles}><i className="fas fa-heart fa-3x"></i></button></a>
+                <a><button onClick= {this.changeStyle.bind(this)} style ={this.state.style}><i className="fas fa-heart fa-3x"></i></button></a>
               </div>
               <div className = "col-md-4 hold card">
                 <img className = "resize" src={sea}/>
                 <p className = "card-text"> I am a text</p>
-                <a><button><i className="fas fa-heart fa-3x"></i></button></a>
+                <a><button onClick= {this.likeThis.bind(this)}><i className="fas fa-heart fa-3x"></i></button></a>
               </div>
               <div className = "col-md-4 hold card">
                 <img className = "resize" src={mountain}/>
                 <p className = "card-text"> I am a text</p>
-                <a><button><i className="fas fa-heart fa-3x"></i></button></a>
+                <a><button onClick= {this.likeThis.bind(this)}><i className="fas fa-heart fa-3x"></i></button></a>
               </div>
               <div className = "col-md-4 hold card">
               <img className = "resize" src={street}/>
               <p className = "card-text"> I am a text</p>
-              <a><button><i className="fas fa-heart fa-3x"></i></button></a>
+              <a><button onClick= {this.likeThis.bind(this)}><i className="fas fa-heart fa-3x"></i></button></a>
             </div>
             <div className = "col-md-4 hold card">
               <img className = "resize" src={sea}/>
               <p className = "card-text"> I am a text</p>
-              <a><button><i className="fas fa-heart fa-3x"></i></button></a>
+              <a><button onClick= {this.likeThis.bind(this)}><i className="fas fa-heart fa-3x"></i></button></a>
             </div>
             <div className = "col-md-4 hold card">
               <img className = "resize" src={mountain}/>
               <p className = "card-text"> I am a text</p>
-              <a><button><i className="fas fa-heart fa-3x"></i></button></a>
+              <a><button onClick= {this.likeThis.bind(this)}><i className="fas fa-heart fa-3x"></i></button></a>
             </div>
             </div>
       
